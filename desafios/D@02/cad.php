f3ecd2b0a6804eb2bd3cf087bdadb20114a03f1f
justrisx <fellipevantine12@gmail.com>
2023-08-21 20:51:44 -0300

    Desafio 2.
    Desafio no qual e necessario randomizar um numero tendo como parametro outros dois.

---
+++ desafios/D@02/cad.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Resultado</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <main>
+        <?php
+                //rand() = 1951 - Linear Congrential Generator
+                //mt_rand() = 1997 - Mersenne Twister
+                //A partir do PHP 7.1, o rand() e um simples apontamento para o mt_rand()
+                // random_int() gera numeros aleatorios criptograficamente seguros([porem e MAIS LENTO, entao usar somente quando realmente necessario])
+
+            $n1 = $_GET["number1"];
+
+            $n2 = $_GET["number2"];
+
+                $rn = rand($n1, $n2);
+                
+              print "<h1>Aleatorizando...</h1>";
+                print "<p>Gerando numero entre $n1 e $n2<p/>";
+                    print "<strong>$rn</strong>";
+                ?>
+    </main>
+</body>
+</html>
