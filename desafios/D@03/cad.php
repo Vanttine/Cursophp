cf3c9b7318d6f7a78bf4f507142d3fa0ce6bf73f
justrisx <fellipevantine12@gmail.com>
2023-08-27 17:55:18 -0300

    Desafio 3

--- desafios/D@03/cad.php
+++ desafios/D@03/cad.php
@@ -6,19 +6,20 @@
     <title>Resultado</title>
     <link rel="stylesheet" href="style.css">
 </head>
-<body>
+<body> 
             <main>
                 <?php 
 
-            $real = $_GET["valor"];
+            $real = $_GET["valor"] ?? 0;
             $dolar = $real / 4.94;
+            $padrao = numfmt_create("us", NumberFormatter::CURRENCY);
 
-            echo "Seus R$" . number_format($real, 2, ",", ".") . " equivalem a U\$D" . number_format($dolar, 2, ",", ".");
+            echo "Seus " . numfmt_format_currency($padrao, $real, "BRL") . " equivalem a " . numfmt_format_currency($padrao, $dolar, "USD");
 
             echo "<p>Codigo feito no dia 22/08/23 com a cotacao atual de 4.94</p>";
                  ?>
                 <a href="javascript:history.go(-1)">Voltar para a pagina anterior</a>
-
+                
             </main>
-</body>
+        </body>
 </html>
