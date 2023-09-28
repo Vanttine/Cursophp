15208004f84a9a8a22dfcd68e543b3bd7b668ec2
justrisx <fellipevantine12@gmail.com>
2023-09-28 19:49:23 -0300

    PHP Challenges.

---
+++ PHP CeV/Desafios/D@05/cad.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="../../CSS/style.css">
+</head>
+<body>
+    <main>
+        <h1>Analisador de Numero Real</h1>
+
+        <?php 
+
+            $num = $_GET["number"] ?? 0;
+        
+            echo "<p>Analisando o numero <strong>". number_format($num, 3, ",", ".") ." </strong>enviado pelo usuario.</p>";
+
+            $int = (int)$num;
+            $fra = $num - $int;
+
+            echo "<ul><li>A parte inteira e ". number_format($int, 0, ",", ".") ."</li><li>A parte fracionaria e ". number_format($fra, 3, ",", ".") ."</li></ul>";
+        ?>
+        <a href="javascript:history.go(-1)">Voltar para a pagina anterior</a> 
+
+    </main>
+</body>
+</html>
