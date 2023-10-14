b7894090e2edc2d5084d22f55768315d700aa28e
justrisx <fellipevantine12@gmail.com>
2023-10-14 20:47:30 -0300

    Activity-4

---
+++ PHP Udemy/ATV/exercise-4/index.php
@@ -0,0 +1,14 @@
+
+<?php
+
+        $n1 = 5;
+       
+        
+        if(is_float($n1)) {
+            echo "Its a float";
+        }else {
+            echo "Its an integer";
+        }
+
+   
+?>
