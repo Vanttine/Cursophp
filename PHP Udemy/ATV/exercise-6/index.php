6e0b5508e59abc39f5b16fc9cdd9e9dd16b1078a
justrisx <fellipevantine12@gmail.com>
2023-10-19 21:55:41 -0300

    Activity-6

---
+++ PHP Udemy/ATV/exercise-6/index.php
@@ -0,0 +1,15 @@
+<?php 
+    $arrsAss = [
+        'type' => 'coupe', 
+        'engine' => 'v12',
+        'wheels' => 4,
+        'brand' => "mercedes",
+        'hp' => 630,
+        'turbo' => true
+    ];
+
+    echo $arrsAss['type'];
+        echo "</br>";
+    
+        print_r($arrsAss);
+?>
